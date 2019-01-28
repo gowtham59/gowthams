ee87e00e238e83da998d8ee8c21d9ef61196cc1b
gowtham59 <47033355+gowtham59@users.noreply.github.com>
2019-01-28 20:55:40 +0530

    Create sign.c

---
+++ sign.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+if(a>0)
+{
+printf("%d is positive");
+}
+else if(a<0)
+{
+printf("%d is negative");
+}
+else
+{
+printf("Zero");
+}
+getch();
+}
